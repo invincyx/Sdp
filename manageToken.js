57da240482002188280c0b3021f1ac8954f71f83
Tatenda <40611131+invincyx@users.noreply.github.com>
2024-06-17 20:19:34 +0200

    chore: Update manageToken.js to use HTTPS for token retrieval

--- manageToken.js
+++ manageToken.js
@@ -31,6 +31,8 @@ export async function getToken() {
 
 try {
     const data  = await fetch("https://10.10.11.162:9480/token/", requestOptions);
+
+    console.log("Request Options: ", requestOptions);
     
     if (data.headers.get('Content-Type').includes('application/json')) {
         const response = await data.json();
