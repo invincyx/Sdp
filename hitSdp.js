78c520b55f5484fa61ab7f1d87aec24f5d49f317
Tatenda <40611131+invincyx@users.noreply.github.com>
2024-06-18 07:51:24 +0200

    Refactor hitSdp function to use correct URL parameter for SDP request

--- hitSdp.js
+++ hitSdp.js
@@ -83,5 +83,5 @@ export async function hitSDP({token, request, requestId, msisdn, planId} ) {
 
 }
 
-hitSDP({token:"eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJTaGFua2x5MTM1IiwiYXVkIjoiQSIsInNjb3BlcyI6IkFETUlOIiwiZW50aXR5SWQiOiIiLCJpc3MiOiJodHRwOi8vc2l4ZGVlLmNvbSIsImlhdCI6MTcxODY4OTQ1NCwiZXhwIjoxNzE4Njk1NDU0fQ.EvWNiTioZbhMRbeYV2M4hBygQCj-QkFOGhGS7tGMnO6c5NXnX8NjVPyd4VhTa8DviNyggSXg_Qz9znxnKXUcGg", request: "Activation", requestId: "123", msisdn: "263774599141", planId: "9913510095"}).then(console.log).catch(console.error);
+hitSDP({token:"eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJTaGFua2x5MTM1IiwiYXVkIjoiQSIsInNjb3BlcyI6IkFETUlOIiwiZW50aXR5SWQiOiIiLCJpc3MiOiJodHRwOi8vc2l4ZGVlLmNvbSIsImlhdCI6MTcxODY4OTQ1NCwiZXhwIjoxNzE4Njk1NDU0fQ.EvWNiTioZbhMRbeYV2M4hBygQCj-QkFOGhGS7tGMnO6c5NXnX8NjVPyd4VhTa8DviNyggSXg_Qz9znxnKXUcGg", request: "Deactivation", requestId: "123", msisdn: "263774599141", planId: "9913510095"}).then(console.log).catch(console.error);
 
