a2b603c953a50aaee43ea0de056781b330941d48
Tatenda <40611131+invincyx@users.noreply.github.com>
2024-06-18 08:48:15 +0200

    Refactor hitSdp function to update the resultCode variable to '88' for successful hits

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ app.get("/test-connection", async (req, res) => {
             // await fs.appendFile('billingHits.txt', `Date: ${new Date().toISOString()} Billing Hit: ${JSON.stringify(hitSdp)}\n`);
   
             // Update status based on the result code
-            const resultCode = await hitSdp.resultCode === "0" ? '11' : hitSdp.resultCode;
+            const resultCode = await hitSdp.resultCode === "0" ? '88' : hitSdp.resultCode;
 
             console.log("🍀 Result code: ", await resultCode);
   
@@ -72,7 +72,7 @@ app.get("/test-connection", async (req, res) => {
   
             // Get the output value
             const result = await database.query("SELECT @output as output");
-            console.log(result[0].output);
+            console.log(result);
           } catch (error) {
             console.log("🍀 Error: ", error);
             console.error(error);
