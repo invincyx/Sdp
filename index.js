3dbf910fe1e1f8e17abd380856a26457d85b6714
Tatenda <40611131+invincyx@users.noreply.github.com>
2024-06-18 09:24:33 +0200

    Refactor hitSdp function to update the procedure call parameters

--- index.js
+++ index.js
@@ -68,7 +68,7 @@ app.get("/test-connection", async (req, res) => {
             await database.query("SET @output = ''");
   
             // Pass the output variable to the procedure
-            const callPocedure = await database.query("CALL SDP_Response(trans_id, receiver, status, @output)", [row.trans_id, row.sender, resultCode]);
+            const callPocedure = await database.query("CALL SDP_Response(?, ?, ?, @output)", [row.trans_id, row.sender, resultCode]);
 
             console.log("🍀 Call Procedure: ", callPocedure);
   
