143a654dfe60162c06006bd568efe94002532212
Tatenda <40611131+invincyx@users.noreply.github.com>
2024-06-18 08:39:46 +0200

    Refactor hitSdp function to use correct URL parameter for SDP request

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ app.get("/test-connection", async (req, res) => {
   
           try {
             console.log("Trying to hit SDP...🍀");
-            const hitSdp = await hitSDP({token: token.access_token, request: featureId, requestId: row.trans_id, msisdn: row.sender, planId: "9913510095" })
+            const hitSdp = await hitSDP({token: token.access_token, request: "Activation", requestId: row.trans_id, msisdn: row.sender, planId: "9913510095" })
             console.log(await hitSdp);
   
             // Log the billing hit
